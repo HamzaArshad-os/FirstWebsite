f48e0811978be9a9200858f2d75d5557811ded41
HamzaArshad-os <71600985+HamzaArshad-os@users.noreply.github.com>
2021-06-21 14:20:04 +0100

    Create Javascript.js

---
+++ Javascript.js
@@ -0,0 +1,36 @@
+const images = document.querySelectorAll(".carousel");
+      let i = 0;
+
+      setInterval(function(){
+        myFunctiongoForward()
+        
+        }, 2000);
+
+      
+      setInterval(myFunctiongoForward, 2000); 
+      var myFunctiongoBack = function(){//works
+        if(i != 0 ){
+          images[i].style.display = 'none';
+          images[i-1].style.display = 'block';
+          i--;
+        }
+        else{
+          images[i].style.display = 'none';
+          images[images.length-1].style.display = 'block'; 
+          i = images.length -1 ; //try it now!
+        }
+      }
+        
+    var myFunctiongoForward = function(){//fixed
+      if(i != images.length-1){ 
+        images[i].style.display = 'none';
+        images[i+1].style.display = 'block';
+        i++;
+      }
+      else{
+        images[i].style.display = 'none';
+        images[0].style.display = 'block';
+        i = 0;
+      }
+      
+    }
